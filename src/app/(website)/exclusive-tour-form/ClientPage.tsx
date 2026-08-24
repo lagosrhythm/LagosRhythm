e7f630469192e8d0ba060f1feab9dad8bdb2792e
Benedict258 <isaacbenedict001@gmail.com>
2026-08-24 15:50:46 +0100

    Update tour date min/max to 5th September 2025

--- src/app/(website)/exclusive-tour-form/ClientPage.tsx
+++ src/app/(website)/exclusive-tour-form/ClientPage.tsx
@@ -30,8 +30,8 @@ export default function Page() {
   const [showConfirmationModal, setShowConfirmationModal] = useState<boolean>(false);
   const maxParticipantCount = populationAmount
   const [loading, setLoading] = useState(false)
-  const minDate = new Date("2026-08-29");
-  const maxDate = new Date("2026-08-29");
+  const minDate = new Date("2025-09-05");
+  const maxDate = new Date("2025-09-05");
   // maxDate.setDate(maxDate.getDate() + 30)
   const [selectedDates, setSelectedDates] = useState<Date[]>([]);
   const [showPaymentModal, setShowPaymentModal] = useState(false)
