97a2e02d2e828af204a9c1c4093fc6f1f0739fd8
Benedict258 <isaacbenedict001@gmail.com>
2026-08-24 12:49:35 +0100

    Hardcode discountCode default to Ruthina on exclusive tour form

--- src/app/(website)/exclusive-tour-form/ClientPage.tsx
+++ src/app/(website)/exclusive-tour-form/ClientPage.tsx
@@ -40,7 +40,6 @@ export default function Page() {
   const [isNigeria, setIsNigeria] = useState(false)
   const searchParams = useSearchParams()
   const referralFromUrl = searchParams?.get("ref") ?? ""
-  const discountFromUrl = searchParams?.get("discount") ?? ""
   // const [timeOptions, setTimeOptions] = useState<customSelectTypes[] | null>(null)
   const formatted = useMemo(() => {
     return selectedDates.map((d) => d.toISOString());
@@ -66,7 +65,7 @@ export default function Page() {
       termsAgreement: false,
       referralSource: referralFromUrl,
       time: "",
-      discountCode: discountFromUrl,
+      discountCode: "Ruthina",
     },
   })
 
